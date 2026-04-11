6bcfd8b7038d5f8f1ceef3da79b5fd851c1e06f7
balramkumar7090 <balram7970kumar@gmail.com>
2026-04-12 00:34:52 +0530

    backend updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import addressRoutes from  "./routes/address.routes.js"
 const app=express();
 const port=process.env.PORT || 4000;
 
-const allowedOrigins=["http://localhost:5173"];
+const allowedOrigins=["https://grocery-app-frontend-zeta.vercel.app/"];
 
 app.use(express.json())
 app.use(cors({
